da6c41376254a22bb60e5420ec1e7a7d6634f6d2
setiwon <45044735+setiwon@users.noreply.github.com>
2023-11-30 20:02:04 -0800

    Added functionality to play button. (added Game.kt)

---
+++ app/src/main/java/com/android/wordhunt/Game.kt
@@ -0,0 +1,13 @@
+package com.android.wordhunt
+
+import android.app.Activity
+import android.os.Bundle
+
+class Game: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
