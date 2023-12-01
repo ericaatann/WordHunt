b2bcc93d3188f5b52b580dcef39eb20ac3604a78
setiwon <45044735+setiwon@users.noreply.github.com>
2023-11-30 19:56:21 -0800

    Added functionality to play button.

--- app/src/main/java/com/android/wordhunt/MainActivity.kt
+++ app/src/main/java/com/android/wordhunt/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.android.wordhunt
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -17,8 +19,24 @@ import com.android.wordhunt.ui.theme.MyApplicationTheme
 import com.google.firebase.inappmessaging.model.Button
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var playButton: android.widget.Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_title_menu)
+
+        playButton = findViewById(R.id.playButton)
+
+        // OnClickListener for the Play button
+        playButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View) {
+                // Handle the click event
+                // Start the MainActivity
+                val intent = Intent(this@MainActivity, Game::class.java)
+                startActivity(intent)
+                Log.d("TitleMenuActivity", "Play button clicked")
+            }
+        })
     }
 }
