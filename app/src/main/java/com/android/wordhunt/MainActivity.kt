2b820b8d8097d39f3c69c9fc63b17886685545b8
Nathan Wang <114364542+Forstandan@users.noreply.github.com>
2023-11-30 18:20:38 -0800

    Update MainActivity.kt
    Make main activity display title screen

--- app/src/main/java/com/android/wordhunt/MainActivity.kt
+++ app/src/main/java/com/android/wordhunt/MainActivity.kt
@@ -19,58 +19,6 @@ import com.google.firebase.inappmessaging.model.Button
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-
-        val gridButtonHandler = gridButtonHandler()
-
-        val button00: View = findViewById(R.id.button00)
-        button00.setOnClickListener(gridButtonHandler.onClickListener(0, 0))
-
-        val button01: View = findViewById(R.id.button01)
-        val button02: View = findViewById(R.id.button02)
-        val button03: View = findViewById(R.id.button03)
-
-        val button10: View = findViewById(R.id.button10)
-        val button11: View = findViewById(R.id.button11)
-        val button12: View = findViewById(R.id.button12)
-        val button13: View = findViewById(R.id.button13)
-
-        val button20: View = findViewById(R.id.button20)
-        val button21: View = findViewById(R.id.button21)
-        val button22: View = findViewById(R.id.button22)
-        val button23: View = findViewById(R.id.button23)
-
-        val button30: View = findViewById(R.id.button30)
-        val button31: View = findViewById(R.id.button31)
-        val button32: View = findViewById(R.id.button32)
-        val button33: View = findViewById(R.id.button33)
-
-
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_title_menu)
     }
 }
