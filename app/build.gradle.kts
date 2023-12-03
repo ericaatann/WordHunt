8661259d22e9383a19a9d228321b31b2131e18e2
ericaatann <ericatan@cpp.edu>
2023-12-02 16:07:14 -0800

    added titlemenuactivity fragment and updated gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
@@ -61,8 +62,10 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("com.google.firebase:firebase-inappmessaging-ktx:20.4.0")
     implementation ("androidx.navigation:navigation-fragment-ktx:2.4.1")
-    implementation ("androidx.navigation:navigation-ui-ktx:2.4.1")
-    implementation("androidx.databinding:databinding-runtime:8.1.4")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.7.5")
+    implementation("androidx.databinding:databinding-runtime:8.2.0")
+    implementation ("androidx.activity:activity-ktx:1.3.1")
+    implementation ("androidx.fragment:fragment-ktx:1.4.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
