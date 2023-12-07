53801ad38e660295a7fe7d85bff1e76caaad52e5
Forstandan <114364542+Forstandan@users.noreply.github.com>
2023-12-06 21:35:43 -0800

    Add backend for fragment game over and navigation for it

--- app/src/main/java/com/android/wordhunt/FragmentGameOver.kt
+++ app/src/main/java/com/android/wordhunt/FragmentGameOver.kt
@@ -1,5 +1,24 @@
 package com.android.wordhunt
 
-class FragmentGameOver {
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
+class FragmentGameOver : Fragment(R.layout.fragment_gameover) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // retry button
+        val retryButton : Button? = view.findViewById(R.id.retry_button)
+//        val menuButton : Button? = view.findViewById(R.id.)
+
+        retryButton?.setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentMenu_to_gameFragment)
+        }
+
+//        menuButton?.setOnClickListener {
+//            findNavController().navigate(R.id.action_fragmentGameOver_to_fragmentMenu)
+//        }
+    }
 }
