826b567780af5f884ed69d753f6b45d18a7986fe
ericaatann <ericatan@cpp.edu>
2023-12-06 21:10:42 -0800

    delete gradient and updated bg to all the same

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,8 +67,6 @@ dependencies {
     implementation("androidx.databinding:databinding-runtime:8.2.0")
     implementation ("androidx.activity:activity-ktx:1.3.1")
     implementation ("androidx.fragment:fragment-ktx:1.4.0")
-    implementation ("com.github.bumptech.glide:glide:4.12.0")
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
